3e1a791bc0e4c2c559dbdbfbb92ebd87ac359024
William.os4y <william.os4y@gmail.com>
2011-02-22 19:54:36 +0100

    release version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='ssdm',
-      version='0.1',
+      version='0.2',
       description='Simple Sqlite Data Mapper',
       author='William',
       author_email='william.os4y@gmail.com',
